1a283869fe8d816ed45bcfc5107fff9b3e96c0dc
Ben Dotte <bdotte@widen.com>
2011-08-10 10:58:48 -0500

    simplify singleton

--- src/main/webapp/js/singleton.js
+++ src/main/webapp/js/singleton.js
@@ -4,25 +4,25 @@ var BallGame = {
 	mbWithBall : undefined,
 
 	throwBall : function() {
-		if (BallGame.mbWithBall.id === BallGame.config.dancingId) {
-			BallGame.mbWithBall = $(BallGame.config.nonDancingId);
+		if (mbWithBall.id === config.dancingId) {
+			mbWithBall = $(config.nonDancingId);
 		}
 		else {
-			BallGame.mbWithBall = $(BallGame.config.dancingId);
+			mbWithBall = $(config.dancingId);
 		}
 
-		var moveXBy = BallGame.mbWithBall.offsetLeft - BallGame.greenBall.offsetLeft;
-		var moveYBy = BallGame.mbWithBall.offsetTop - BallGame.greenBall.offsetTop;
+		var moveXBy = mbWithBall.offsetLeft - greenBall.offsetLeft;
+		var moveYBy = mbWithBall.offsetTop - greenBall.offsetTop;
 
-		new Effect.Move(BallGame.greenBall, {x: moveXBy, y: moveYBy, duration: 1});
+		new Effect.Move(greenBall, {x: moveXBy, y: moveYBy, duration: 1});
 	},
 
 	moveBeaverNBall : function(event) {
-		var moveXBy = event.clientX - BallGame.mbWithBall.offsetLeft;
-		var moveYBy = event.clientY - BallGame.mbWithBall.offsetTop;
+		var moveXBy = event.clientX - mbWithBall.offsetLeft;
+		var moveYBy = event.clientY - mbWithBall.offsetTop;
 
-		new Effect.Move(BallGame.mbWithBall, {x: moveXBy, y: moveYBy, duration: 1});
-		new Effect.Move(BallGame.greenBall, {x: moveXBy, y: moveYBy, duration: 1});
+		new Effect.Move(mbWithBall, {x: moveXBy, y: moveYBy, duration: 1});
+		new Effect.Move(greenBall, {x: moveXBy, y: moveYBy, duration: 1});
 	},
 
 	handleClick : function(event) {
@@ -32,8 +32,8 @@ var BallGame = {
 			return;
 		}
 
-		if ((eventElement.id === BallGame.config.dancingId && BallGame.mbWithBall.id !== BallGame.config.dancingId) ||
-			(eventElement.id === BallGame.config.nonDancingId && BallGame.mbWithBall.id !== BallGame.config.nonDancingId)) {
+		if ((eventElement.id === config.dancingId && mbWithBall.id !== config.dancingId) ||
+			(eventElement.id === config.nonDancingId && mbWithBall.id !== config.nonDancingId)) {
 			BallGame.throwBall();
 			return;
 		}
@@ -42,16 +42,16 @@ var BallGame = {
 	},
 
 	init : function(conf) {
-		BallGame.config = conf;
+		config = conf;
 
-		BallGame.greenBall = $(BallGame.config.ballId);
-		BallGame.mbWithBall = $(BallGame.config.dancingId);
+		greenBall = $(config.ballId);
+		mbWithBall = $(config.dancingId);
 
 		document.observe('click', function(event) {
 			BallGame.handleClick(event);
 		});
 	}
-};
+}
 
 document.observe('dom:loaded', function() {
 	BallGame.init({
